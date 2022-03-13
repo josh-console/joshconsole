43d1869af867f46cf24e8276239961e6c62f4385
Josh Console <josh.console@ef.com>
2022-03-12 19:08:50 -0500

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-5NL9XQCSN2"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+        
+                    gtag('config', 'GA_MEASUREMENT_ID');
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
